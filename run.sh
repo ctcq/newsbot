a62b2257835716f153a6503114b4d81f5c2e6081
Christopher Wiedey <c_wied05@uni-muenster.de>
2020-03-28 22:59:54 +0100

    added docker start script

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker build -t newsfeed_bot .
-docker run -it --rm --name nb -v /root/newsfeed_bot:/opt/newsfeed_bot newsfeed_bot python /opt/newsfeed_bot/src/main.py
+docker run -it --rm --name nb -v /root/newsfeed_bot:/opt/newsfeed_bot newsfeed_bot python /opt/newsfeed_bot/src/main.py -d
